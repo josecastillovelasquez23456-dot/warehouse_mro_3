d7d005bf5affbb7c7b88c90391a5084f3ef5ea84
josecastillovelasquez23456-dot <josecastillovelasquez23456@gmail.com>
2025-12-08 16:52:04 -0500

    Update bultos_routes.py

--- routes/bultos_routes.py
+++ routes/bultos_routes.py
@@ -2,7 +2,8 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 
 # IMPORTS CORRECTOS PARA RAILWAY
-from models.bultos import PostRegistro
+from models.bultos import Bulto
+from models.post_registro import PostRegistro
 from models import db
 
 from datetime import datetime
@@ -185,5 +186,6 @@ def historial_post():
     historial = PostRegistro.query.order_by(PostRegistro.fecha_registro.desc()).all()
 
     return render_template("bultos/historial_post.html", historial=historial)
+
 
 
