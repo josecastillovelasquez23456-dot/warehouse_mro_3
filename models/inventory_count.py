91487a9c2733cf812f6aec1b8364c0db6d41f994
josecastillovelasquez23456-dot <josecastillovelasquez23456@gmail.com>
2025-12-09 17:53:49 -0500

    Update inventory_count.py

--- models/inventory_count.py
+++ models/inventory_count.py
@@ -1,11 +1,18 @@
 from models import db
 from datetime import datetime
 
+
 class InventoryCount(db.Model):
     __tablename__ = "inventory_count"
 
     id = db.Column(db.Integer, primary_key=True)
-    material_code = db.Column(db.String(50), nullable=False)
-    location = db.Column(db.String(50), nullable=False)
+
+    material_code = db.Column(db.String(50), nullable=False, index=True)
+    location = db.Column(db.String(50), nullable=False, index=True)
+
     real_count = db.Column(db.Integer, nullable=False)
-    fecha = db.Column(db.DateTime, default=datetime.now)
+
+    fecha = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<InventoryCount {self.material_code} @ {self.location}>"
