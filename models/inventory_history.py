f38d91638d1d232fa7aae34511a998f863a222d4
josecastillovelasquez23456-dot <josecastillovelasquez23456@gmail.com>
2025-12-09 17:54:05 -0500

    Update inventory_history.py

--- models/inventory_history.py
+++ models/inventory_history.py
@@ -1,6 +1,7 @@
 from models import db
 from datetime import datetime
 
+
 class InventoryHistory(db.Model):
     __tablename__ = "inventory_history"
 
@@ -9,13 +10,14 @@ class InventoryHistory(db.Model):
     snapshot_id = db.Column(db.String(64), nullable=False, index=True)
     snapshot_name = db.Column(db.String(150), nullable=False)
 
-    material_code = db.Column(db.String(50), nullable=False)
+    material_code = db.Column(db.String(50), nullable=False, index=True)
     material_text = db.Column(db.String(255), nullable=False)
     base_unit = db.Column(db.String(20), nullable=False)
-    location = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(50), nullable=False, index=True)
 
     libre_utilizacion = db.Column(db.Float, default=0)
+
     creado_en = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"<InventoryHistory {self.snapshot_name} - {self.material_code}>"
+        return f"<History {self.snapshot_name} - {self.material_code}>"
